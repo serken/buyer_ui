137e111eb6a6b9918c2a33ae9495731f01b935f9
Pavel Kharchenko <serken13@gmail.com>
2019-06-13 13:06:21 +0300

    Install babel\

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import store from "./js/store/index";
-import App from "./js/components/App.jsx";
+import "regenerator-runtime/runtime"
+import React from "react"
+import { render } from "react-dom"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from "react-redux"
+import store from "./js/store/index"
+import App from "./js/components/App.jsx"
 
 render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>,
-  document.getElementById("root")
-);
+  document.getElementById("app")
+)
+
+module.hot.accept()
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+}
+

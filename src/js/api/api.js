aff70f0f4bedc1ee77e0bb2547a7886357ce4a0d
Pavel Kharchenko <serken13@gmail.com>
2019-06-15 17:50:42 +0300

    Add api js

---
+++ src/js/api/api.js
@@ -0,0 +1,40 @@
+const apiUrl = 'http://localhost:3000/'
+
+function makeRequest(endpoint, method = 'GET', body = null) {
+  let params = {
+      method: method,
+      credentials: 'include', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        }
+    }
+  if(body) {
+    params.body = JSON.stringify(body)
+  }
+  return fetch(apiUrl + endpoint,params)
+    .then(response =>
+      response.json()
+    ).catch((error) => {});
+}
+
+export const getUsers = function() {
+  return makeRequest('users')
+}
+
+export const restore = function(payload) {
+  return makeRequest('auth')
+}
+
+export const createUserRequest = function(payload) {
+  return makeRequest('users', 'POST', payload)
+}
+
+export const signIn = function(payload) {
+  return makeRequest('auth', 'POST', payload)
+}
+
+export const signOut = function() {
+  return makeRequest('auth', 'DELETE')
+}
--- src/js/sagas/api-saga.js
+++ src/js/sagas/api-saga.js
@@ -1,4 +1,5 @@
 import { takeEvery, call, put, push } from "redux-saga/effects"
+import * as api from './../api/api.js'
 import { AUTH_REQUESTED,
   AUTH_RECEIVED,
   LOGOUT_REQUESTED,
@@ -20,95 +21,26 @@ export default function* watcherSaga() {
 }
 
 function* processSignIn(action) {
-  const response = yield call(signIn, action.payload)
+  const response = yield call(api.signIn, action.payload)
   yield put({ type: AUTH_RECEIVED, response: response })
 }
 
 function* processSignOut(action) {
-  const response = yield call(signOut, action.payload)
+  const response = yield call(api.signOut, action.payload)
   yield put({ type: LOGOUT_RECEIVED, response: response })
 }
 
 function* restoreSession(action) {
-  const response = yield call(restore)
+  const response = yield call(api.restore)
   yield put({ type: SESSION_RESTORE_RECEIVED, response: response })
 }
 
 function* createUser(action) {
-  const response = yield call(createUserRequest, action.payload)
+  const response = yield call(api.createUserRequest, action.payload)
   yield put({ type: CREATE_USER_RECEIVED, response: response })
 }
 
 function* fetchUsers() {
-  const response = yield call(getUsers)
+  const response = yield call(api.getUsers)
   yield put({ type: USERS_RECEIVED, response: response })
-}
-
-function getUsers() {
-  return fetch("http://localhost:3000/users",
-    {
-      method: 'GET',
-      credentials: 'include', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        }
-    }).then(response =>
-    response.json()
-  ).catch((error) => {});
-}
-
-function restore(payload) {
-  return fetch("http://localhost:3000/auth",
-    {
-      method: 'GET',
-      credentials: 'include', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        }
-    }).then(response =>
-    response.json()
-  ).catch((error) => {});
-}
-
-function createUserRequest(payload) {
-  return fetch("http://localhost:3000/users",
-    {
-      method: 'POST',
-      body: JSON.stringify(payload),
-      credentials: 'include', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      }
-    }).then(response =>
-    response.json()
-  ).catch((error) => {});
-}
-
-function signIn(payload) {
-  return fetch("http://localhost:3000/auth",
-    {
-      method: 'POST',
-      body: JSON.stringify(payload),
-      credentials: 'include', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      }
-    }).then(response =>
-    response.json()
-  ).catch((error) => {});
-}
-
-function signOut () {
-  return fetch('http://localhost:3000/auth', {
-    method: 'DELETE',
-    credentials: 'include'
-  }).then(res => res.json()).catch((error) => {})
 }
